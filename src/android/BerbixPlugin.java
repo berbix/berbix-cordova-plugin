8dd5724d9eb1705672bd1edd034752936d9fd655
Eric Levine <eric@berbix.com>
2019-08-27 17:28:24 -0700

    Support Berbix Verify for Android.

---
+++ src/android/BerbixPlugin.java
@@ -0,0 +1,77 @@
+package com.berbix.cordova;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.PermissionHelper;
+
+import com.berbix.berbixverify.BerbixSDK;
+import com.berbix.berbixverify.BerbixSDKAdapter;
+import com.berbix.berbixverify.BerbixSDKOptions;
+import com.berbix.berbixverify.BerbixSDKOptionsBuilder;
+
+public class BerbixPlugin extends CordovaPlugin implements BerbixSDKAdapter {
+    private static final String VERIFY = "verify";
+
+    private CallbackContext callbackContext;
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        JSONObject config = args.optJSONObject(0);
+
+        if (config == null) {
+            callbackContext.error("cannot start berbix flow configuration");
+            return true;
+        }
+
+        String clientID = config.optString("client_id");
+        String templateKey = config.optString("template_key");
+        String baseURL = config.optString("base_url");
+        String clientToken = config.optString("client_token");
+        String environment = config.optString("environment");
+
+        if (clientID == null) {
+            callbackContext.error("cannot start berbix flow without client ID");
+            return true;
+        }
+
+        BerbixSDKOptionsBuilder options = new BerbixSDKOptionsBuilder();
+
+        if (!templateKey.isEmpty()) {
+            options = options.setTemplateKey(templateKey);
+        }
+        if (!baseURL.isEmpty()) {
+            options = options.setBaseURL(baseURL);
+        }
+        if (!clientToken.isEmpty()) {
+            options = options.setClientToken(clientToken);
+        }
+
+        BerbixSDK sdk = new BerbixSDK(clientID);
+        final BerbixSDKOptions sdkOptions = options.build();
+
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                sdk.startFlow(BerbixPlugin.this.cordova.getActivity(), BerbixPlugin.this, sdkOptions);
+            }
+        });
+
+        this.callbackContext = callbackContext;
+
+        return true;
+    }
+
+    @Override
+    public void onComplete() {
+        this.callbackContext.success();
+    }
+
+    @Override
+    public void onError(Throwable error) {
+        this.callbackContext.error(error.toString());
+    }
+}
